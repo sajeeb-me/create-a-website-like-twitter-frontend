c7e10b30d355fef8dcf7d5e43c8169766dfcd96f
sdshuvo9898 <hello.sdshuvo@gmail.com>
2022-09-13 15:48:30 +0600

    implemented google sign up method

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
 import twitterImage from '../../assets/images/twitter.jpeg';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth'
 import auth from '../../firebase.init';
+import GoogleButton from 'react-google-button'
+import { Link, useNavigate } from 'react-router-dom';
+import './Login.css'
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     // const [errorMessage, setError] = useState('');
+    const navigate = useNavigate();
+
     const [
         signInWithEmailAndPassword,
         user,
@@ -15,8 +20,12 @@ const Login = () => {
         error,
     ] = useSignInWithEmailAndPassword(auth);
 
-    if (user) {
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+
+    if (user || googleUser) {
+        navigate('/')
         console.log(user)
+        console.log(googleUser)
     }
 
     if (error) {
@@ -32,32 +41,61 @@ const Login = () => {
         console.log(email, password);
         signInWithEmailAndPassword(email, password);
     }
+    const handleGoogleSignIn = () => {
+        signInWithGoogle();
+    }
+
 
     return (
         <div className='login-container'>
             <div className="image-container">
-                <img src={twitterImage} alt="" />
+                <img className='image' src={twitterImage} alt="" />
             </div>
             <div className="form-container">
-                <TwitterIcon />
-                <h2>Happing now</h2>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="email"
-                        className='email'
-                        placeholder='Email address'
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <input
-                        type="password"
-                        className='password'
-                        placeholder='Password'
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <div className='btn-login'>
-                        <button type='submit' className='btn'>Login</button>
+                <div className='form-box'>
+                    <TwitterIcon style={{ color: 'skyblue' }} />
+                    <h2 className='heading'>Happing now</h2>
+                    <h3 className='heading1'>What happening today</h3>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="email"
+                            className='email'
+                            placeholder='Email address'
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            className='password'
+                            placeholder='Password'
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <div className='btn-login'>
+                            <button type='submit' className='btn'>Login</button>
+                        </div>
+                    </form>
+                    <hr />
+                    <div className='google-button'>
+                        <GoogleButton
+                            className='g-btn'
+                            type='light'
+                            onClick={handleGoogleSignIn}
+                        />
                     </div>
-                </form>
+                </div>
+                <div>
+                    Don't have account?
+                    <Link
+                        to='/signup'
+                        style={{
+                            textDecoration: 'none',
+                            color: 'skyblue',
+                            fontWeight: '600',
+                            marginLeft: '5px'
+                        }}
+                    >
+                        Sign up
+                    </Link>
+                </div>
             </div>
         </div>
     );
--- src/pages/Login/Signup.js
+++ src/pages/Login/Signup.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import twitterImage from '../../assets/images/twitter.jpeg';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth'
 import auth from '../../firebase.init';
+import GoogleButton from 'react-google-button'
+import { Link, useNavigate } from 'react-router-dom';
+import './Login.css'
 
 const Signup = () => {
     const [username, setUsername] = useState('');
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     const [
         createUserWithEmailAndPassword,
@@ -17,8 +21,12 @@ const Signup = () => {
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
 
-    if (user) {
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+
+    if (user || googleUser) {
+        navigate('/')
         console.log(user)
+        console.log(googleUser)
     }
 
     if (error) {
@@ -34,44 +42,72 @@ const Signup = () => {
         console.log(email, password);
         createUserWithEmailAndPassword(email, password);
     }
+    const handleGoogleSignIn = () => {
+        signInWithGoogle();
+    }
 
     return (
-        <div className='signup-container'>
+        <div className='login-container'>
             <div className="image-container">
-                <img src={twitterImage} alt="" />
+                <img className='image' src={twitterImage} alt="" />
             </div>
             <div className="form-container">
-                <TwitterIcon />
-                <h2>Happing now</h2>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        className='display-name'
-                        placeholder='@username'
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <input
-                        type="text"
-                        className='display-name'
-                        placeholder='Enter full name'
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <input
-                        type="email"
-                        className='email'
-                        placeholder='Email address'
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <input
-                        type="password"
-                        className='password'
-                        placeholder='Password'
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <div className='btn-login'>
-                        <button type='submit' className='btn'>Sign up</button>
+                <div className='form-box'>
+                    <TwitterIcon className='Twittericon' style={{ color: 'skyblue' }} />
+                    <h2 className='heading'>Happing now</h2>
+                    <h3 className='heading1'>Join twitter today</h3>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            className='display-name'
+                            placeholder='@username'
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            className='display-name'
+                            placeholder='Enter full name'
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <input
+                            type="email"
+                            className='email'
+                            placeholder='Email address'
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            className='password'
+                            placeholder='Password'
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <div className='btn-login'>
+                            <button type='submit' className='btn'>Sign up</button>
+                        </div>
+                    </form>
+                    <hr />
+                    <div className='google-button'>
+                        <GoogleButton
+                            className='g-btn'
+                            type='light'
+                            onClick={handleGoogleSignIn}
+                        />
                     </div>
-                </form>
+                </div>
+                <div>
+                    Already have an account?
+                    <Link
+                        to='/login'
+                        style={{
+                            textDecoration: 'none',
+                            color: 'skyblue',
+                            fontWeight: '600',
+                            marginLeft: '5px'
+                        }}
+                    >
+                        Login
+                    </Link>
+                </div>
             </div>
         </div>
     );
