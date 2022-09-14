50f3e141a0c10f16788beaf4894869c6290541ef
sdshuvo9898 <hello.sdshuvo@gmail.com>
2022-09-14 15:55:14 +0600

    image upload method

--- src/pages/Feed/TweetBox.js
+++ src/pages/Feed/TweetBox.js
@@ -1,15 +1,54 @@
 import { Avatar, Button } from '@mui/material';
 import React, { useState } from 'react';
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import axios from 'axios'
 import './TweetBox.css'
 
 const TweetBox = () => {
     const [post, setPost] = useState('');
     const [imageURL, setImageURL] = useState('');
+    const [isLoading, setIsLoading] = useState('');
+
+
+    const handleUploadImage = (e) => {
+        setIsLoading(true);
+        const image = e.target.files[0];
+
+        const formData = new FormData();
+        formData.set('image', image)
+
+        axios.post("https://api.imgbb.com/1/upload?key=59207bb0bb404bee12b87515615fe979", formData)
+            .then(res => {
+                setImageURL(res.data.data.display_url)
+                console.log(res.data.data.display_url)
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.log(error);
+                setIsLoading(false);
+            })
+    }
 
     const handleTweet = (e) => {
         e.preventDefault();
-        console.log(post);
+        if (imageURL) {
+            const userPost = {
+                post: post,
+                photo: imageURL
+            }
+            console.log(userPost);
+            fetch(`http://localhost:5000/post`, {
+                method: "POST",
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(userPost)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                })
+        }
     }
 
     return (
@@ -25,12 +64,15 @@ const TweetBox = () => {
                 </div>
                 <div className="imageIcon_tweetButton">
                     <label htmlFor="image" className='imageIcon'>
-                        <AddPhotoAlternateIcon />
+                        {
+                            isLoading ? <p>Uploading image</p> : <p>{imageURL ? 'image uploaded' : <AddPhotoAlternateIcon />}</p>
+                        }
                     </label>
                     <input
                         type="file"
                         id='image'
                         className='imageInput'
+                        onChange={handleUploadImage}
                     />
                     <Button className='tweetBox_tweetButton' type='submit'>Tweet</Button>
                 </div>
